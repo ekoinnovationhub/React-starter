eba6ab086f24d1f3b1452aa746a0098c62369afb
Adekoreday <khord4eng@gmail.com>
2020-06-24 17:58:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+ # React Starter Template for EkoInnovationhub
+
+ Includes the following
+
+ - [x] Prod/dev webpack config
+ - [x] unit test setup using jest
+ - [x] E2E test setup using Nightwatch.js
+
---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <div className="app">
+
+      <ul class="actions special">
+        <li>
+            hello
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
